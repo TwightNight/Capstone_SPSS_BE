20d11dfb922d453490a73649aab3dd1584d99b34
Dyan8386 <phamdinhngancc151@gmail.com>
2025-10-31 23:19:28 +0700

    FIx ApiGateway

---
+++ SPSS_ProductOrderService/SPSS.BusinessObject/DTOs/Brand/BrandResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SPSS.BusinessObject.DTOs.Brand
+{
+    public class BrandResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public int CountryId { get; set; }
+        public string? CountryName { get; set; }
+    }
+}
---
+++ SPSS_ProductOrderService/SPSS.BusinessObject/DTOs/Brand/UpdateBrandRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPSS.BusinessObject.DTOs.Brand
+{
+    public class UpdateBrandRequest
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Brand name is required.")]
+        [StringLength(100, ErrorMessage = "Brand name cannot be longer than 100 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Brand title is required.")]
+        [StringLength(200, ErrorMessage = "Brand title cannot be longer than 200 characters.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Brand description is required.")]
+        [StringLength(500, ErrorMessage = "Brand description cannot be longer than 500 characters.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Image URL is required.")]
+        [StringLength(500, ErrorMessage = "Image URL cannot be longer than 500 characters.")]
+        [Url(ErrorMessage = "Please enter a valid URL for the image.")]
+        public string ImageUrl { get; set; }
+
+        [Required(ErrorMessage = "Country ID is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a valid country.")]
+        public int CountryId { get; set; }
+    }
+}
